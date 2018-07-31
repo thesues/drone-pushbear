030865de54d18a000a849da0ca2e60c34d9d6e01
Dongmao Zhang <deanraccoon@gmail.com>
2018-07-31 19:51:06 +0800

    Test OK

--- index.js
+++ index.js
@@ -5,24 +5,33 @@ if (!process.env.SENDKEY) {
   console.log("no send key");
   process.exit(2);
 } else {
-  var sendkey = process.env.SENDKEY;
+  var sendkey = process.env.SENDKEY.trim();
 }
 
-var name = process.env.DRONE_REPO_NAME;
-var status = process.env.DRONE_BUILD_STATUS;
-var commit = process.env.DRONE_COMMIT_SHA;
-var link = process.env.DRONE_BUILD_LINK;
+var name = process.env.DRONE_REPO_NAME|| "";
+var status = process.env.DRONE_BUILD_STATUS|| "";
+var commit = process.env.DRONE_COMMIT_SHA || "";
+var link = process.env.DRONE_BUILD_LINK || "" ;
+var tag = process.env.DRONE_TAG || "";
 
+var encodedText;
+if (tag === "") {
+  encodedText  = encodeURIComponent(`${name}的构建结果是${status}`);
+} else {
+  encodedText  = encodeURIComponent(`${name}的构建结果是${status},TAG是${tag}`);
+}
 
-  var encodedText  = encodeURIComponent(`Building of ${name} is ${status}`);
+  var encodedText  = encodeURIComponent(`${name}的构建结果是${status},TAG是`);
   var encodedDesp =  encodeURIComponent(`
   ## ${link}: ${status}
 
   + commit: ${commit}
   `);
 
+var url=`https://pushbear.ftqq.com/sub?sendkey=${sendkey}&text=${encodedText}&desp=${encodedDesp}`;
+console.log(url);
 request({
-  uri: `https://pushbear.ftqq.com/sub?sendkey=${sendkey}&text=${encodedText}&desp=${encodedDesp}`,
+  uri: url,
   method: 'GET',
 }, (err, response, body) => {
   if (!err && response.statusCode === 200) {
